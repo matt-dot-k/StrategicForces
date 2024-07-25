0e97327ae16f391e947c7f036dc75e270bdd05d7
matt-dot-k <matthewkielar16@gmail.com>
2024-07-24 21:51:49 -0700

    change UI, add interactive chart

--- app.R
+++ app.R
@@ -1,92 +1,124 @@
-require(tidyverse)
 require(shiny)
-require(ggthemes)
 require(bslib)
+require(tidyverse)
+require(RColorBrewer)
 
-# Read data for triad legs ---
+source("theme.R")
 
-air_forces <- list(
-    systems = read.csv("data/air_systems.csv"),
-    warheads = read.csv("data/air_warheads.csv")
-)
+# Load data --------
+strategicForces <- read.csv("./data/strategicForces.csv")
 
-ggplot(
-    data = air_forces$systems %>% as_tibble(),
-    aes(x = Year, y = Number, color = System)
-) +
-    geom_line()
-
-land_forces <- list(
-    systems = read.csv("data/land_systems.csv"),
-    warheads = read.csv("data/land_warheads.csv")
-)
-
-sea_forces <- list(
-    systems = read.csv("data/sea_systems.csv"),
-    warheads = read.csv("data/sea_warheads.csv")
-)
-
-# Define UI ----
-
-ui <- page_sidebar(
-    title = "U.S. Strategic Nuclear Forces",
-    sidebar = sidebar(
-        selectInput(
-            inputId = "triadLeg",
-            label = "Select a Triad Leg",
-            c("Air", "Land", "Sea")
-        ),
-        selectInput(
-            inputId = "deployment",
-            label = "Select a Deployment",
-            c("Delivery Vehicles", "Warheads")
-        ),
-        sliderInput(
-            inputId = "Year",
-            label = "Select a Date Range",
-            min = 1945,
-            max = 2023,
-            value = c(1945, 2023)
+ui <- fluidPage(
+    fluidRow(
+        column(3,
+            fluidRow(
+                column(9, wellPanel(
+                    style = "height: 250px",
+                    p(tags$em("Click on a point on the plot for more details")),
+                    htmlOutput("info_click")))),
+            fluidRow(
+                column(9, wellPanel(
+                    selectInput(
+                        inputId = "triadLeg",
+                        label = "Select a Triad Leg",
+                        c("Air", "Land", "Sea")),
+                    selectInput(
+                        inputId = "deployment",
+                        label = "Select a Deployment",
+                        c("Delivery Vehicle", "Warhead")),
+                    sliderInput(
+                        inputId = "year",
+                        label = "Select a Date Range",
+                        min = 1945, max = 2023,
+                        value = c(1945, 2023), sep = ""))))),
+        column(9, wellPanel(
+            plotOutput("forcesChart", height = 720, click = "infoClick"))
         )
-    ),
-    mainPanel(
-        plotOutput("plot")
     )
 )
 
 server <- function(input, output) {
-    output$plot <- renderPlot({
-        data <- switch(
-            input$triadLeg,
-            "Air" = air_forces,
-            "Land" = land_forces,
-            "Sea" = sea_forces
-        )
-        data <- switch(
-            input$deployment,
-            "Delivery Vehicles" = data$systems,
-            "Warheads" = data$warheads
-        )
+
+    # Get data from UI settings --------
+    getData <- reactive({
+        req(input$triadLeg)
+        req(input$deployment)
+        req(input$year)
+
+        data <- strategicForces %>%
+            filter(
+                Leg == input$triadLeg & Type == input$deployment) %>%
+            filter(
+                Year >= input$year[1] & Year <= input$year[2])
+    })
+
+    # Draw chart --------
+    output$forcesChart <- renderPlot({
         legend_title <- switch(
             input$deployment,
-            "Delivery Vehicles" = "Platform",
-            "Warheads" = "Warhead"
-        )
+            "Delivery Vehicle" = "Platform",
+            "Warhead" = "Warhead")
+            
         ggplot(
-            data = data,
+            data = getData(),
             aes(x = Year, y = Number, color = System)
         ) +
-        geom_point(
-            size = 1.2
+            geom_point(
+                size = 2.0
         ) +
-        geom_line(
-            linewidth = 1.2
+            geom_line(
+                linewidth = 2.0
         ) +
-        labs(
-            x = "Year",
-            y = "Number",
-            color = legend_title
+            labs(
+                x = "Year", y = "Number", color = legend_title
         )
+    })
+
+    # Get chart info --------
+    output$info_click <- renderPrint({
+
+        info_str <- function(click) {
+                        
+            info <- if(nrow(click) == 0) {
+                if (input$deployment == "Delivery Vehicle") {
+                    p(tags$b("System: "), br(),
+                      tags$b("Range: "), br(),
+                      tags$b("CEP: "), br(),
+                      tags$b("Guidance: "), br(),
+                      tags$b("Payload: "), br())
+                } else if(input$deployment == "Warhead") {
+                    p(tags$b("System: "), br(),
+                      tags$b("Designer: "), br(),
+                      tags$b("Mass: "), br(),
+                      tags$b("Detonation Mechanism: "), br(),
+                      tags$b("Blast Yield: "), br())
+                }
+            } else if(nrow(click >= 0)) {
+                if (input$deployment == "Delivery Vehicle") {
+                    p(tags$b("System: ",), tags$em(click$System), br(),
+                      tags$b("Range: "), tags$em(click$Info_1), br(),
+                      tags$b("CEP: "), tags$em(click$Info_2), br(),
+                      tags$b("Guidance: "), tags$em(click$Info_3), br(),
+                      tags$b("Payload: "), tags$em(click$Info_4), br())
+                } else if(input$deployment == "Warhead") {
+                    p(tags$b("System: "), tags$em(click$System), br(),
+                      tags$b("Designer: "), tags$em(click$Info_1), br(),
+                      tags$b("Mass: "), tags$em(click$Info_2), br(),
+                      tags$b("Detonation Mechanism: "), tags$em(click$Info_3), br(),
+                      tags$b("Blast Yield: "), tags$em(click$Info_4), br())
+                }
+            } else {
+                NA
+            }               
+            return(info)
+        }
+
+        info_str(nearPoints(
+            getData() %>%
+                filter(Leg == input$triadLeg & Type == input$deployment) %>%
+                filter(Year >= input$year[1] & Year <= input$year[2]),
+            coordinfo = input$infoClick) %>%
+            select(System, Info_1, Info_2, Info_3, Info_4))
     })
 }
 
